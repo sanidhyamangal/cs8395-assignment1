7ae70c829c23793d6ff71c49286594a54af1f506
Sanidhya Mangal <mangalsanidhya19@gmail.com>
2021-11-14 01:23:10 -0600

    created eigen values (#1)
    * added: dataset for working
    
    * feat: added project scaffolding
    
    * feat: added base test for the file building
    
    * fixed: return error
    
    * fixed: semi colon error
    
    * feat: added ops for loading file
    
    feat: added sanity check for getting largest region size
    
    * fixed: image pointer issue
    
    * fixed: method chaining issue
    
    * fixed: method access
    
    * updated: added nrrd data file
    
    * feat: added eigen value and eigen vector
    
    updated: the input and output cmd line args
    added: method for performing eigen value computation
    
    * fixed: datatype for the img typedef
    
    * fixed: eigenvector matrix typedef
    
    * fixed: eigenvector allocator
    
    updated: allocation for the eigenvector, instead of direct allocation multiply it with 1
    
    * fixed: allocation error
    
    updated: thisTensor typedef to TensorType instead of TensorImageType
    
    * fixed: vector typedef
    
    updated: vector typedef to itk::Vector
    
    * fixed: typo eigen analysis
    
    * fixed: eigen computation errors
    
    fixed: input image reader name pointer
    fixed: eigenmatrix typo
    fixed: method for setting to eigen value and vector
    
    * fixed: input reader pipeline
    
    * fixed: missing semicolon error
    
    * fixed: typedef for input image reader
    
    * fixed: paImageIterator issue
    
    * reverted: to og
    
    * removed: update paImageIterator
    
    * added: additional image iterator
    
    * added: additional image iterator
    
    * test: code
    
    * test: code
    
    * test: code
    
    * test: code
    
    * test: code
    
    * rewamped:code
    
    * fixed: input typedef error
    
    * fixed: input typedef error
    
    * test: code
    
    * fixed: issues
    
    * fixed: issues
    
    * fixed: issues
    
    * fixed: typedef issues
    
    * fixed: typedef issues
    
    * reformat: code

---
+++ Tactography/Tactography.cxx
@@ -0,0 +1,95 @@
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkDiffusionTensor3D.h"
+#include "itkSymmetricEigenAnalysisImageFilter.h"
+#include "itkTensorFractionalAnisotropyImageFilter.h"
+#include "itkVector.h"
+#include "itkImageIterator.h"
+
+//---Image Typedefs--//
+const unsigned int nDims = 3 ;   // Setup types
+typedef itk::DiffusionTensor3D < double > TensorType ;
+typedef itk::Image < TensorType , nDims > ImageType ;
+typedef itk::Vector <double, nDims> VectorType;
+typedef itk::Image <VectorType, nDims> PAImageType ;
+typedef itk::Image < double, nDims> BaseImageType ;
+typedef itk::ImageFileReader < ImageType > TensorImageFileReaderType ;
+typedef itk::ImageRegionIterator < ImageType > InputImageIterator ;
+typedef itk::ImageRegionIterator < PAImageType > PAImageIterator ;
+TensorType thisTensor ;
+TensorType::EigenValuesArrayType eigenValArrayType;
+TensorType::EigenVectorsMatrixType eigenValMatrixType ;
+VectorType thisVector ;
+
+int main ( int argc, char * argv[] )
+{
+  // --- Verify command line arguments----//
+  if( argc < 6 )
+    {
+      std::cerr << "Usage: " << std::endl ;
+      std::cerr << argv[0] << " inputFileImage inputSegmentFile outputEigenVecFile outputFAImage outputSegmentTrackFile" << std::endl ; 
+      return -1 ;
+    }
+
+  
+  // read input image file
+  TensorImageFileReaderType ::Pointer inputFileReader = TensorImageFileReaderType ::New() ;  
+  inputFileReader->SetFileName ( argv[1] ) ;
+  inputFileReader->Update();
+
+  // define the pointer for image reader
+  ImageType::Pointer img = inputFileReader->GetOutput();
+
+  // compute new region which would help in computation of the eigen values and vector
+  ImageType::RegionType newRegion ; // define new region for iterator
+
+  ImageType::SizeType size = img->GetLargestPossibleRegion().GetSize() ;
+  ImageType::IndexType origin ; // create a new index pointing to origin
+  origin[0] = 0; origin[1] = 0; origin[2] = 0;
+
+  newRegion.SetSize( size ) ;
+  newRegion.SetIndex( origin ) ;
+
+
+  // define PA image for other ops
+  PAImageType::Pointer paImage = PAImageType::New() ;
+  paImage->SetOrigin(img->GetOrigin() ) ;
+  paImage->SetDirection(img->GetDirection() );
+  paImage->SetSpacing(img->GetSpacing() );
+  paImage->SetRegions(newRegion);
+  paImage->Allocate() ;
+
+  PAImageIterator paIterator (paImage, newRegion);
+  InputImageIterator inputImageIterator (img, newRegion);
+  paIterator.GoToBegin ();
+  inputImageIterator.GoToBegin () ;
+
+  while (!paIterator.IsAtEnd() )
+  {
+   thisTensor =  inputImageIterator.Value() ;
+   thisTensor.ComputeEigenAnalysis(eigenValArrayType, eigenValMatrixType) ;
+
+   // compute and store eigenValue in this tensor to update it to next iterator
+   thisVector[0] = eigenValMatrixType[2][0]*1 ; 
+   thisVector[1] = eigenValMatrixType[2][1]*1 ; 
+   thisVector[2] = eigenValMatrixType[2][2]*1 ;
+
+
+  // change the vector to zero for making it to zero for the out of bound eigen values
+   if (eigenValMatrixType[2][2] == 1) { 
+     thisVector[0] = 0; 
+     thisVector[1] = 0; 
+     thisVector[2] = 0;
+   }
+
+   // update the iterators for making it better 
+   paIterator.Set(thisVector) ;
+   ++paIterator ;
+   ++inputImageIterator ;
+  }
+
+  // Done.
+  return 0 ;
+}
+
